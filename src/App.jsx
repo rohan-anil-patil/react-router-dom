ab9a8029339145daf515cedc1001f0c299bb0706
rohan-anil-patil <rohanpatil1216@gmail.com>
2025-01-13 08:48:42 +0530

    file setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline  bg-black text-white text-center">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
